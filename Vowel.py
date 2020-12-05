8c8bc5e28ac8edb4dca836a1b7749b9f55e6ddbf
Vaibhav Parsana <50186193+vaibhavparsana@users.noreply.github.com>
2020-12-05 16:28:46 -0500

    Add files via upload

---
+++ Vowel.py
@@ -0,0 +1,6 @@
+vowels = ['a','e','i','o','u']
+word = "Use Your Editor When Working on More Than a Few Lines of Code"
+for letter in word:
+    if letter in vowels:
+        print(letter)
+        
